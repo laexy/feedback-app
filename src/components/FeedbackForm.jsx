f221debe21b4e56a8553fdbc3ed2567d9b6a14a6
laexy <pots_e@pm.me>
2022-05-02 13:56:56 +1000

    Added State hooks for btnDisabled and message + validation (msg <=10 characters)

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,8 +6,21 @@ import Button from "./shared/Button";
 function FeedbackForm() {
   // setting text state
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (event) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters.");
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
     setText(event.target.value);
   };
 
@@ -23,8 +36,11 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <Button type='submit' version='primary'>Send</Button>
+          <Button type="submit" version="primary" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
