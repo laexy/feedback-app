5ebf42a0e687d17ca2735765e1ac1c390cfe0dbf
laexy <pots_e@pm.me>
2022-05-04 21:52:42 +1000

    Added spinner to context w/ gif and isLoading for when feedback is loading Section 7.5

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
@@ -7,28 +7,28 @@ const FeedbackContext = createContext();
 // children are components that need access to our context
 // any functions are passed into a prop of 'value'
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is feedback item 1.",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This is feedback item 2.",
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: "This is feedback item 3.",
-      rating: 7,
-    },
-  ]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [feedback, setFeedback] = useState([]);
 
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   });
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  // fetch feedback from database
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+    const data = await response.json();
+
+    setFeedback(data);
+    setIsLoading(false);
+  };
 
   // delete feedback
   const deleteFeedback = (id) => {
@@ -66,6 +66,7 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
         editFeedback,
         updateFeedback,
         addFeedback,
